b86184a17211c863424ad33492ce719149c316e7
Robert Painsi <robertpainsi@gmail.com>
2023-05-15 12:56:34 +0200

    [#25] Add sass support for examples
    - Create shared css file for examples. Closes #26.
    - Add animation to shared css file. Closes #24.

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,41 @@
 //webpack.config.js
-import path from "path";
+import path from 'path';
 import {fileURLToPath} from 'url';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-    mode: "production",
-    target: "web",
-    // devtool: "inline-source-map",
+    mode: 'production',
+    target: 'web',
+    // devtool: 'inline-source-map',
     entry: {
-        main: "./src/index.ts",
+        index: './src/index.ts',
+        'example-style': './examples/style/index.scss',
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: "index.js",
-        libraryTarget: "umd",
-        globalObject: "this",
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        globalObject: 'this',
         umdNamedDefine: true,
     },
     resolve: {
-        extensions: [".ts", ".js"],
+        extensions: ['.ts', '.js'],
     },
     module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: "ts-loader"
-            }
-        ]
-    }
+        rules: [{
+            test: /\.ts$/,
+            loader: 'ts-loader'
+        }, {
+            test: /\.(s(a|c)ss)$/,
+            use: [/*'style-loader'*/ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
 };
