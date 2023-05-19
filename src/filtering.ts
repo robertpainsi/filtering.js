26d0f2754697a51f29bbd08458c7126a463cdb07
Robert Painsi <robertpainsi@gmail.com>
2023-05-19 12:24:21 +0200

    Replace TypeScript's private keyword with JavaScript's #

--- src/filtering.ts
+++ src/filtering.ts
@@ -4,39 +4,39 @@ import {findOne} from "./utils";
 
 export class Filtering {
 
-    private readonly schema: Schema;
-    private readonly options: FilteringOptions;
+    readonly #schema: Schema;
+    readonly #options: FilteringOptions;
 
     constructor(data: Schema, options = {}) {
-        this.schema = data;
-        this.options = options;
+        this.#schema = data;
+        this.#options = options;
     }
 
     filter(filterData: FilterData): Result {
-        const result = this.createEmptyResult(this.schema);
+        const result = this.#createEmptyResult(this.#schema);
 
         let relevantItems: Item[] = [];
-        if (this.options.filterItem) {
-            for (const item of this.schema.items) {
-                if (this.options.filterItem(item, this.schema, filterData)) {
+        if (this.#options.filterItem) {
+            for (const item of this.#schema.items) {
+                if (this.#options.filterItem(item, this.#schema, filterData)) {
                     relevantItems.push(item);
                 }
             }
         } else {
-            relevantItems = [...this.schema.items];
+            relevantItems = [...this.#schema.items];
         }
         for (const item of relevantItems) {
             result.addAllItem(item);
         }
-        for (const item of this.getFilterItems(relevantItems, filterData)) {
+        for (const item of this.#getFilterItems(relevantItems, filterData)) {
             result.addFilteredItem(item);
         }
-        this.calculatePossibleItems(result, relevantItems, filterData);
+        this.#calculatePossibleItems(result, relevantItems, filterData);
 
         return result;
     }
 
-    private createEmptyResult(schema: Schema) {
+    #createEmptyResult(schema: Schema) {
         const result = new Result(schema);
         for (const group of schema.groups) {
             const groupResult = new GroupResult(group);
@@ -49,12 +49,12 @@ export class Filtering {
         return result;
     }
 
-    private calculatePossibleItems(result: Result, items: Item[], filterData: FilterData) {
+    #calculatePossibleItems(result: Result, items: Item[], filterData: FilterData) {
         for (const groupName of result.groupNames) {
             const possibleFilterData = filterData.clone();
             possibleFilterData.disableGroup(groupName);
 
-            const filteredItems = this.getFilterItems(items, possibleFilterData);
+            const filteredItems = this.#getFilterItems(items, possibleFilterData);
             for (const filteredItem of filteredItems) {
                 for (const filteredItemFilterName of filteredItem.getFilterNames(groupName)) {
                     result.getGroup(groupName).getFilter(filteredItemFilterName).addPossibleItem(filteredItem);
@@ -63,7 +63,7 @@ export class Filtering {
         }
     }
 
-    private getFilterItems(items: Item[], filterData: FilterData) {
+    #getFilterItems(items: Item[], filterData: FilterData) {
         const result = new Set<Item>();
 
         for (const item of items) {
